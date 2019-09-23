17f8c025481498be721111f3020d4b51f7477323
wang0759 <wang0759@algonquinlive.com>
2019-09-22 21:13:43 -0400

    add new code'listPrimes'

---
+++ listPrimes.py
@@ -0,0 +1,16 @@
+
+def isprime(n):
+    if n <= 1:
+        return False
+    for x in range(2, n):
+        if n % x == 0:
+            return False
+    else:
+        return True
+
+def list_primes():  
+    for n in range(100):
+        if isprime(n):
+            print(n,end=" ")
+    print()
+list_primes()
